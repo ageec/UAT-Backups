00b9d3f5e3f3e7baa8584719451ada3c3a38b54c
ageec <>
2026-06-24 19:51:41 +0000

    [Copado] Clone of 20260618 UAT Rel

---
+++ lwc/publicHealthWashUAnalyticsQuickLinks/publicHealthWashUAnalyticsQuickLinks.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class PublicHealthWashUAnalyticsQuickLinks extends LightningElement {}
---
+++ lwc/samFoxAnalyticsQuickLinks/samFoxAnalyticsQuickLinks.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class SamFoxAnalyticsQuickLinks extends LightningElement {}
